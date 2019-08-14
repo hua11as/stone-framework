3aff9430c789310a49f7b201851b8e008f372538
as <88452486@qq.com>
2019-08-14 21:16:19 +0800

    init

---
+++ stone-micro-service/src/main/java/com/meyacom/stone/configuration/MicroServiceConfiguration.java
@@ -0,0 +1,16 @@
+package com.meyacom.stone.configuration;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * desc:
+ * author: as
+ * date: 2019/8/14
+ */
+@Configuration
+@JsonFormat
+public class MicroServiceConfiguration {
+
+
+}
